5bebb09d472ed9211d481d3461c690d11e911c8c
源治 <caiyurichen@126.com>
2026-07-07 17:02:48 +0800

    Fix Supabase project URL

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.BABYBINGO_SUPABASE = {
-  url: "https://clisychhwoavbbqxdvyf.supabase.co/rest/v1/",
+  url: "https://clisychhwoavbbqxdvyf.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsaXN5Y2hod29hdmJicXhkdnlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4MjMxMjQsImV4cCI6MjA5ODM5OTEyNH0.IVHyTUI_qemyTDPsGTQDXMTx-HPwqZfYn3zosAEA-tk",
   bucket: "babybingo-photos",
   stateTable: "babybingo_state",
